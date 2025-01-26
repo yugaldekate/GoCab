b5f2c2b073b422d35eb9abb50c6129da8f9d08e9
Yugal D Dekate <yugaldekate72@gmail.com>
2025-01-26 23:11:15 +0530

    created modals for verify OTP and verification successful, implemented clerk auth

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,12 +1,26 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Link } from 'expo-router';
+import { Text, View } from 'react-native';
+import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo';
 
 const Home = () => {
+    const { user } = useUser();
+
     return (
-        <SafeAreaView>
-            <Text>Home</Text>
-        </SafeAreaView>
-    );
-};
+        <View>
+            <SignedIn>
+                <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+            </SignedIn>
+
+            <SignedOut>
+                <Link href="/(auth)/sign-in">
+                    <Text>Sign in</Text>
+                </Link>
+                <Link href="/(auth)/sign-up">
+                    <Text>Sign up</Text>
+                </Link>
+            </SignedOut>
+        </View>
+    )
+}
 
 export default Home;
